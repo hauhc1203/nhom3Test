46e5bb871ac2c602db21560de7ba922ed2966943
hauhc1203 <hau.hc1203@gmail.com>
2022-07-05 09:44:07 +0700

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Hello extends JFrame {
+
+    public Hello() throws HeadlessException {
+        Container   container=this.getContentPane();
+        container.setLayout(new BorderLayout());
+        container.add(new JLabel("Hello World"), BorderLayout.CENTER);
+        setSize(500,400);
+        setLocationRelativeTo(null);
+        setTitle("Hello");
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args)  {
+       new Hello();
+    }
+}
